552e74ad4067382795d3aa66c083a918e9482e94
andy dmitriev <andydmitriev@users.noreply.github.com>
2021-08-09 12:05:33 +0300

    cleanup + comments

--- backend/src/main/java/com/smartech/i2019/adaptiveinterviews/controller/DepartmentController.java
+++ backend/src/main/java/com/smartech/i2019/adaptiveinterviews/controller/DepartmentController.java
@@ -6,13 +6,17 @@ import com.smartech.i2019.adaptiveinterviews.util.DepartmentForm;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import java.util.List;
 
@@ -26,47 +30,61 @@ public class DepartmentController {
 
     @Operation(summary = "Список всех отделов")
     @GetMapping()
-    ResponseEntity<List<Department>> findAll() {
-        List<Department> departments = departmentService.findAll();
-        return new ResponseEntity<>(departments, HttpStatus.OK);
+    List<Department> findAll() {
+        return departmentService.findAll();
     }
 
     @Operation(summary = "Добавить отдел")
     @PostMapping("/add")
-    ResponseEntity<Department> addDepartment(@RequestBody DepartmentForm departmentForm) {
+    Department addDepartment(@RequestBody DepartmentForm departmentForm) {
         Department department = new Department();
         department.setName(departmentForm.getName());
         department.setSupervisor(departmentForm.getSupervisor());
         departmentService.add(department);
-        return new ResponseEntity<>(department, HttpStatus.OK);
+        return department;
     }
 
+    // FIXME: в этом маппинге не нужно слово "delete"
+    //        достаточно просто @DeleteMapping("/{id}")
+    //
     @Operation(summary = "Удалить отдел")
     @DeleteMapping("/delete/{id}")
     @Secured("ROLE_ADMIN")
-    ResponseEntity<String> deleteDepartment(@PathVariable long id) {
+    String deleteDepartment(@PathVariable long id) {
         departmentService.delete(id);
-        return new ResponseEntity<>("Department deleted", HttpStatus.OK);
+        return "Department deleted";
     }
 
+    // FIXME: в этом маппинге не нужно слово "find"
+    //        достаточно просто @GetMapping("/{id}")
+    //
+    //        также проверка на null толком не работает
+    //        в ответе не приходит локализованная строка
+    //        чуть позже прикрутим обработчик
+    //
     @Operation(summary = "Найти отдел по ID")
     @GetMapping("/find/{id}")
-    ResponseEntity<Department> findDepartment(@PathVariable @Min(1) long id) {
-        Department department = departmentService.findById(id);
-        if (department == null) {
-            throw new EntityNotFoundException("Отдел не найден");
-        }
-        return new ResponseEntity<>(department, HttpStatus.OK);
+    Department findDepartment(@PathVariable @Min(1) long id) {
+//        Department department = departmentService.findById(id);
+//        if (department == null) {
+//            throw new EntityNotFoundException("Отдел не найден");
+//        }
+//        return department;
+
+        return departmentService.findById(id);
     }
 
+    // FIXME: в этом маппинге не нужно слово "update"
+    //        достаточно просто @PutMapping("/{id}")
+    //
     @Operation(summary = "Обновить данные отдела")
     @PutMapping("/update/{id}")
-    ResponseEntity<Department> updateDepartment(@PathVariable long id, @RequestBody DepartmentForm departmentForm) {
+    Department updateDepartment(@PathVariable long id, @RequestBody DepartmentForm departmentForm) {
         Department department = departmentService.findById(id);
         department.setName(departmentForm.getName());
         department.setSupervisor(departmentForm.getSupervisor());
         departmentService.edit(department);
-        return new ResponseEntity<>(department, HttpStatus.OK);
+        return department;
     }
 
 }
